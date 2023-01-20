cc36863c570d8ab84aff448a7881567d778bf93e
Pedro <113485042+Pescobedo1982@users.noreply.github.com>
2023-01-20 00:24:11 -0300

    Update archivo.js

--- scripts/archivo.js
+++ scripts/archivo.js
@@ -2,12 +2,12 @@ const ver = document.querySelector(".first");
 const noVer = document.querySelector(".hidden");
 const usuario = document.querySelector(".mail").value;
 
-document.querySelector(".entrar").addEventListener("click", () => { 
-    document.querySelector(".comprador").innerHTML = "Bienvenido " + usuario   
-    document.querySelector(".first"),ver.style.display = "block";
-    document.querySelector(".hidden"),noVer.style.display = "none";
-});
+document.querySelector(".entrar").addEventListener("click", () => {
+    document.querySelector(".comprador").innerHTML = "Bienvenido" + usuario + "A Chantun";
+    document.querySelector(".first"), ver.style.display = "block"
+    document.querySelector(".hidden"), noVer.style.display = "none"
 
+});
 
 const dolarMep = [{}, { id: 1, nombre: "MEP", precio: 305 }];
 const enJSON = JSON.stringify(dolarMep);
@@ -26,7 +26,6 @@ comprarMep.addEventListener("input", () => {
     if (comprarMep.value >= 61100)
         Swal.fire('No puede comprar mas de 200 dólares MEP')
 
-
 });
 
 let terminarCompra = document.querySelector(".btn")
@@ -35,7 +34,7 @@ terminarCompra.addEventListener("click", () => {
     if (comprarMep.value <= 61100) {
         document.querySelector("#billetera").innerHTML = compra + " dólares MEP"
         Swal.fire({
-            title: '¿Esta seguro que quiere comprar?',
+            title: `¿Esta seguro que quiere comprar?`,
             text: "Recuerde que tiene una compra por sesión",
             icon: 'warning',
             showCancelButton: true,
@@ -49,40 +48,45 @@ terminarCompra.addEventListener("click", () => {
                 button.innerText = 'retirar dólares y reiniciar compra';
                 document.body.appendChild(button)
                 button.addEventListener("click", () => {
-          let timerInterval;
-          Swal.fire({
-            title: "Se está transfiriendo su dinero!",
-            html: "Se reiniciara la aplicación en <b></b> segundos.",
-            timer: 6000,
-            timerProgressBar: true,
-            didOpen: () => {
-              Swal.showLoading();
-              const b = Swal.getHtmlContainer().querySelector("b");
-              timerInterval = setInterval(() => {
-                b.textContent = Swal.getTimerLeft();
-              }, 300);
-            },
-            willClose: () => {
-              clearInterval(timerInterval);
-            },
-          }).then((result) => {
-            /* Read more about handling dismissals below */
-            if (result.dismiss === Swal.DismissReason.timer) {
-              console.log("I was closed by the timer");
+                    let timerInterval
+                    Swal.fire({
+                        title: 'Se está transfiriendo su dinero!',
+                        html: 'Se reiniciara <b></b> segundos.',
+                        timer: 2000,
+                        timerProgressBar: true,
+                        didOpen: () => {
+                            Swal.showLoading()
+                            const b = Swal.getHtmlContainer().querySelector('b')
+                            timerInterval = setInterval(() => {
+                                b.textContent = Swal.getTimerLeft()
+                            }, 100)
+                        },
+                        willClose: () => {
+                            clearInterval(timerInterval)
+                        }
+                    }).then((result) => {
+                        /* Read more about handling dismissals below */
+                        if (result.dismiss === Swal.DismissReason.timer) {
+                            console.log('I was closed by the timer')
+                        }
+                        location.reload()
+                    })
+                    
+                }),
+                    Swal.fire(
+                        document.querySelector("#billetera").innerHTML = "Usted tiene " + compra + " dolares Mep",
+                        terminarCompra.disabled = true
+
+
+
+                    )
             }
-          });
-          location.reload();
-        }),
-          Swal.fire(
-            (document.querySelector("#billetera").innerHTML =
-              "Usted tiene " + compra + " dolares Mep"),
-            "Usted a Comprado " + compra + " Dólares Mep",
-            (terminarCompra.disabled = true)
-          );
-      } else if (true) {
-        document.querySelector("#billetera").innerHTML =
-          "Usted cancelo la compra.";
-      }
-    });
-  }
+            else if (true) {
+                document.querySelector("#billetera").innerHTML = "Usted cancelo la compra."
+
+            }
+        })
+
+    }
+
 });
